b0fc4d41b8ed2fc85704d2411541d64f11c48135
Pankaj-369 <114094352+Pankaj-369@users.noreply.github.com>
2025-08-29 23:18:36 +0530

    "j"

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -122,11 +122,11 @@ const Contact = () => {
                   style={{ height: '100%', width: '100%' }}
                   scrollWheelZoom={false}
                 >
-          <TileLayer
-  attribution='Tiles &copy; Esri &mdash; Source: Esri, DeLorme, NAVTEQ'
-  url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}"
+           <TileLayer
+  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/">CARTO</a>'
+  url="https://basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
+  subdomains={['a','b','c','d']}
 />
-
 
 
 
